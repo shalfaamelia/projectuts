e566cdb34cdb39e5ff6c234b8c02021eda5d1098
Iman Danis R.M <d.imandamaryan@gmail.com>
2024-10-15 10:53:12 +0700

    menghapus viewCompact

--- app/src/main/java/com/shalfa/projectuts/MainActivity.kt
+++ app/src/main/java/com/shalfa/projectuts/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.shalfa.projectuts
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(R.layout.activity_main)
 
-    }
+    val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+    recyclerView.layoutManager = LinearLayoutManager(this)
+
+    // Set adapter with your data
+    recyclerView.adapter = MyAdapter(myDataList)
 }
